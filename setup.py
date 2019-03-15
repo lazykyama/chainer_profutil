1d2108dbe68a2f45f7e17333b8d3c9385916fcd7
Kazuhiro Yamasaki <yamasaki.k.1101@gmail.com>
2019-03-15 14:14:52 +0900

    Add setup script.

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""
+This setup script is based on pypa's template.
+https://github.com/pypa/sampleproject
+"""
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='chainer_profutil',
+    version='0.0.1',
+    description='Chainer utility tool for GPU profiling by nvprof.',
+    author='Kazuhiro Yamasaki',
+    author_email='yamasaki.k.1101@gmail.com',
+    license='MIT License',
+    packages=find_packages(exclude=['docs', 'tests']),
+)
